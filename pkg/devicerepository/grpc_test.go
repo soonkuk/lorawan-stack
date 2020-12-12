207cf448e135cfcd2c96c92544e33245b873ca8d
Aggelos Kolaitis <aggelos.kolaitis@thethingsindustries.com>
2020-12-12 23:58:08 +0200

    dr: Unit tests

--- pkg/devicerepository/grpc_test.go
+++ pkg/devicerepository/grpc_test.go
@@ -15,15 +15,22 @@
 package devicerepository_test
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/gogo/protobuf/types"
+	"github.com/smartystreets/assertions"
 	"go.thethings.network/lorawan-stack/v3/pkg/component"
 	componenttest "go.thethings.network/lorawan-stack/v3/pkg/component/test"
 	"go.thethings.network/lorawan-stack/v3/pkg/config"
 	"go.thethings.network/lorawan-stack/v3/pkg/devicerepository"
 	"go.thethings.network/lorawan-stack/v3/pkg/devicerepository/store"
+	"go.thethings.network/lorawan-stack/v3/pkg/errors"
 	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
 	"go.thethings.network/lorawan-stack/v3/pkg/util/test"
+	"go.thethings.network/lorawan-stack/v3/pkg/util/test/assertions/should"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type mockStore struct {
@@ -121,10 +128,10 @@ func TestGRPC(t *testing.T) {
 	}
 	c := componenttest.NewComponent(t, componentConfig)
 
-	store := &mockStore{}
+	st := &mockStore{}
 	conf := &devicerepository.Config{
-		Store:         store,
-		PhotosBaseURL: "https://assets/",
+		Store:         st,
+		AssetsBaseURL: "https://assets/",
 	}
 	dr, err := devicerepository.New(c, conf)
 	test.Must(dr, err)
@@ -135,8 +142,402 @@ func TestGRPC(t *testing.T) {
 	cc := dr.LoopbackConn()
 	cl := ttnpb.NewDeviceRepositoryClient(cc)
 
+	t.Run("ListBrands", func(t *testing.T) {
+		t.Run("Request", func(t *testing.T) {
+			a := assertions.New(t)
+
+			_, err := cl.ListBrands(test.Context(), &ttnpb.ListEndDeviceBrandsRequest{
+				Limit:   100,
+				Page:    2,
+				OrderBy: "brand_id",
+				Search:  "query string",
+				FieldMask: types.FieldMask{
+					Paths: []string{"lora_alliance_vendor_id"},
+				},
+			})
+			a.So(err, should.BeNil)
+			a.So(st.lastGetBrandsRequest, should.Resemble, store.GetBrandsRequest{
+				BrandID: "",
+				Limit:   100,
+				Page:    2,
+				OrderBy: "brand_id",
+				Paths:   []string{"lora_alliance_vendor_id", "brand_id"},
+				Search:  "query string",
+			})
+		})
+
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			brands, err := cl.ListBrands(test.Context(), &ttnpb.ListEndDeviceBrandsRequest{})
+			a.So(brands, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.brands = []*ttnpb.EndDeviceBrand{
+				{
+					BrandID:                 "brand1",
+					PrivateEnterpriseNumber: 100,
+					Logo:                    "item.png",
+				},
+				{
+					BrandID: "brand2",
+				},
+			}
+			st.err = nil
+
+			responseHeaders := metadata.MD{}
+			brands, err := cl.ListBrands(test.Context(), &ttnpb.ListEndDeviceBrandsRequest{}, grpc.Header(&responseHeaders))
+			a.So(err, should.BeNil)
+			a.So(brands, should.Resemble, &ttnpb.ListEndDeviceBrandsResponse{
+				Brands: []*ttnpb.EndDeviceBrand{
+					{
+						BrandID:                 "brand1",
+						PrivateEnterpriseNumber: 100,
+						Logo:                    "https://assets/vendor/brand1/item.png",
+					},
+					{
+						BrandID: "brand2",
+					},
+				},
+			})
+
+			s := responseHeaders.Get("x-total-count")
+			a.So(s, should.Resemble, []string{"2"})
+		})
+	})
+
+	t.Run("GetBrand", func(t *testing.T) {
+		t.Run("Request", func(t *testing.T) {
+			a := assertions.New(t)
+
+			_, err := cl.GetBrand(test.Context(), &ttnpb.GetEndDeviceBrandRequest{
+				BrandID: "brand1",
+				FieldMask: types.FieldMask{
+					Paths: []string{"lora_alliance_vendor_id"},
+				},
+			})
+			a.So(err, should.BeNil)
+			a.So(st.lastGetBrandsRequest, should.Resemble, store.GetBrandsRequest{
+				Limit:   1,
+				BrandID: "brand1",
+				Paths:   []string{"lora_alliance_vendor_id", "brand_id"},
+			})
+		})
+
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			brands, err := cl.ListBrands(test.Context(), &ttnpb.ListEndDeviceBrandsRequest{})
+			a.So(brands, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.brands = []*ttnpb.EndDeviceBrand{
+				{
+					BrandID:                 "brand1",
+					PrivateEnterpriseNumber: 100,
+					Logo:                    "item.png",
+				},
+			}
+			st.err = nil
+
+			brand, err := cl.GetBrand(test.Context(), &ttnpb.GetEndDeviceBrandRequest{})
+			a.So(err, should.BeNil)
+			a.So(brand, should.Resemble, &ttnpb.EndDeviceBrand{
+				BrandID:                 "brand1",
+				PrivateEnterpriseNumber: 100,
+				Logo:                    "https://assets/vendor/brand1/item.png",
+			})
+		})
+	})
+
+	t.Run("ListModels", func(t *testing.T) {
+		t.Run("Request", func(t *testing.T) {
+			a := assertions.New(t)
+
+			_, err := cl.ListModels(test.Context(), &ttnpb.ListEndDeviceModelsRequest{
+				BrandID: "brand1",
+				Limit:   100,
+				Page:    2,
+				OrderBy: "brand_id",
+				Search:  "query string",
+				FieldMask: types.FieldMask{
+					Paths: []string{"firmware_versions"},
+				},
+			})
+			a.So(err, should.BeNil)
+			a.So(st.lastGetModelsRequest, should.Resemble, store.GetModelsRequest{
+				ModelID: "",
+				BrandID: "brand1",
+				Limit:   100,
+				Page:    2,
+				OrderBy: "brand_id",
+				Paths:   []string{"firmware_versions", "brand_id", "model_id"},
+				Search:  "query string",
+			})
+		})
+
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			res, err := cl.ListModels(test.Context(), &ttnpb.ListEndDeviceModelsRequest{})
+			a.So(res, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.models = []*ttnpb.EndDeviceModel{
+				{
+					BrandID: "brand1",
+					ModelID: "model1",
+					Photos: &ttnpb.EndDeviceModel_Photos{
+						Main:  "a.png",
+						Other: []string{"b.png"},
+					},
+				},
+				{
+					BrandID: "brand2",
+					ModelID: "model2",
+				},
+			}
+			st.err = nil
+
+			responseHeaders := metadata.MD{}
+			brands, err := cl.ListModels(test.Context(), &ttnpb.ListEndDeviceModelsRequest{}, grpc.Header(&responseHeaders))
+			a.So(err, should.BeNil)
+			a.So(brands, should.Resemble, &ttnpb.ListEndDeviceModelsResponse{
+				Models: []*ttnpb.EndDeviceModel{
+					{
+						BrandID: "brand1",
+						ModelID: "model1",
+						Photos: &ttnpb.EndDeviceModel_Photos{
+							Main:  "https://assets/vendor/brand1/a.png",
+							Other: []string{"https://assets/vendor/brand1/b.png"},
+						},
+					},
+					{
+						BrandID: "brand2",
+						ModelID: "model2",
+					},
+				},
+			})
+
+			s := responseHeaders.Get("x-total-count")
+			a.So(s, should.Resemble, []string{"2"})
+		})
+	})
+
+	t.Run("GetModel", func(t *testing.T) {
+		t.Run("Request", func(t *testing.T) {
+			a := assertions.New(t)
+
+			_, err := cl.GetModel(test.Context(), &ttnpb.GetEndDeviceModelRequest{
+				BrandID: "brand1",
+				ModelID: "model1",
+				FieldMask: types.FieldMask{
+					Paths: []string{"firmware_versions"},
+				},
+			})
+			a.So(err, should.BeNil)
+			a.So(st.lastGetModelsRequest, should.Resemble, store.GetModelsRequest{
+				Limit:   1,
+				BrandID: "brand1",
+				ModelID: "model1",
+				Paths:   []string{"firmware_versions", "brand_id", "model_id"},
+			})
+		})
+
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			models, err := cl.ListModels(test.Context(), &ttnpb.ListEndDeviceModelsRequest{})
+			a.So(models, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.models = []*ttnpb.EndDeviceModel{
+				{
+					BrandID: "brand1",
+					ModelID: "model1",
+					Photos: &ttnpb.EndDeviceModel_Photos{
+						Main:  "a.png",
+						Other: []string{"b.png"},
+					},
+				},
+			}
+			st.err = nil
+
+			model, err := cl.GetModel(test.Context(), &ttnpb.GetEndDeviceModelRequest{})
+			a.So(err, should.BeNil)
+			a.So(model, should.Resemble, &ttnpb.EndDeviceModel{
+				BrandID: "brand1",
+				ModelID: "model1",
+				Photos: &ttnpb.EndDeviceModel_Photos{
+					Main:  "https://assets/vendor/brand1/a.png",
+					Other: []string{"https://assets/vendor/brand1/b.png"},
+				},
+			})
+		})
+	})
+
+	t.Run("GetTemplate", func(t *testing.T) {
+		st.template = &ttnpb.EndDeviceTemplate{
+			EndDevice: ttnpb.EndDevice{
+				VersionIDs: ids,
+			},
+			FieldMask: types.FieldMask{
+				Paths: []string{"version_ids"},
+			},
+		}
+
+		t.Run("Request", func(t *testing.T) {
+			a := assertions.New(t)
+			_, err := cl.GetTemplate(test.Context(), ids)
+			a.So(err, should.BeNil)
+			a.So(st.lastVersionIDs, should.Resemble, ids)
+		})
+
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			models, err := cl.GetTemplate(test.Context(), ids)
+			a.So(models, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = nil
+
+			template, err := cl.GetTemplate(test.Context(), ids)
+			a.So(err, should.BeNil)
+			a.So(template, should.Resemble, st.template)
+		})
+	})
+
+	t.Run("Authenticate", func(t *testing.T) {
+		// TODO authenticate with rights
+	})
+
+	t.Run("GetUplinkDecoder", func(t *testing.T) {
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			models, err := cl.GetTemplate(test.Context(), ids)
+			a.So(models, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+			a.So(st.lastVersionIDs, should.Resemble, ids)
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.uplinkDecoder = "uplink decoder"
+			st.err = nil
+
+			c, err := cl.GetUplinkDecoder(test.Context(), ids)
+			a.So(err, should.BeNil)
+			a.So(c, should.Resemble, &ttnpb.MessagePayloadFormatter{
+				Formatter:          ttnpb.PayloadFormatter_FORMATTER_JAVASCRIPT,
+				FormatterParameter: "uplink decoder",
+			})
+		})
+
+		t.Run("ClusterAuth", func(t *testing.T) {
+			conf.RequireAuth = true
+			codec, err := cl.GetUplinkDecoder(test.Context(), ids)
+			a := assertions.New(t)
+			a.So(errors.IsUnauthenticated(err), should.BeTrue)
+			a.So(codec, should.BeNil)
+
+			_, err = cl.GetUplinkDecoder(test.Context(), ids, c.WithClusterAuth())
+			a.So(err, should.BeNil)
+
+			conf.RequireAuth = false
+		})
+	})
+
+	t.Run("GetDownlinkDecoder", func(t *testing.T) {
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			models, err := cl.GetTemplate(test.Context(), ids)
+			a.So(models, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+			a.So(st.lastVersionIDs, should.Resemble, ids)
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.downlinkDecoder = "downlink decoder"
+			st.err = nil
+
+			c, err := cl.GetDownlinkDecoder(test.Context(), ids)
+			a.So(err, should.BeNil)
+			a.So(c, should.Resemble, &ttnpb.MessagePayloadFormatter{
+				Formatter:          ttnpb.PayloadFormatter_FORMATTER_JAVASCRIPT,
+				FormatterParameter: "downlink decoder",
+			})
+		})
+
+		t.Run("ClusterAuth", func(t *testing.T) {
+			conf.RequireAuth = true
+			codec, err := cl.GetDownlinkDecoder(test.Context(), ids)
+			a := assertions.New(t)
+			a.So(errors.IsUnauthenticated(err), should.BeTrue)
+			a.So(codec, should.BeNil)
+
+			_, err = cl.GetDownlinkDecoder(test.Context(), ids, c.WithClusterAuth())
+			a.So(err, should.BeNil)
+			conf.RequireAuth = false
+		})
+	})
+
+	t.Run("GetDownlinkEncoder", func(t *testing.T) {
+		t.Run("StoreError", func(t *testing.T) {
+			a := assertions.New(t)
+			st.err = fmt.Errorf("store error")
+			models, err := cl.GetTemplate(test.Context(), ids)
+			a.So(models, should.BeNil)
+			a.So(err.Error(), should.ContainSubstring, st.err.Error())
+			a.So(st.lastVersionIDs, should.Resemble, ids)
+		})
+
+		t.Run("Success", func(t *testing.T) {
+			a := assertions.New(t)
+			st.downlinkEncoder = "downlink encoder"
+			st.err = nil
+
+			c, err := cl.GetDownlinkEncoder(test.Context(), ids)
+			a.So(err, should.BeNil)
+			a.So(c, should.Resemble, &ttnpb.MessagePayloadFormatter{
+				Formatter:          ttnpb.PayloadFormatter_FORMATTER_JAVASCRIPT,
+				FormatterParameter: "downlink encoder",
+			})
+		})
+
+		t.Run("ClusterAuth", func(t *testing.T) {
+			conf.RequireAuth = true
+			codec, err := cl.GetDownlinkEncoder(test.Context(), ids)
+			a := assertions.New(t)
+			a.So(errors.IsUnauthenticated(err), should.BeTrue)
+			a.So(codec, should.BeNil)
+
+			_, err = cl.GetDownlinkEncoder(test.Context(), ids, c.WithClusterAuth())
+			a.So(err, should.BeNil)
+			conf.RequireAuth = false
+		})
+	})
+
 	// conf is device repository config
-	// store is device repository store
+	// st is device repository store
 	// cl is device repository client
 
 	// TODO: tests
